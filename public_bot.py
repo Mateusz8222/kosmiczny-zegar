d5c7a7bcbb468a029035f2e3e989c84aebb8c41f
Mateusz8222 <witam.matt@gmail.com>
2026-03-11 16:00:47 +0100

    Update public_bot.py

--- public_bot.py
+++ public_bot.py
@@ -18,25 +18,24 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Zalogowano jako {bot.user}")
 
+    try:
+        synced = await bot.tree.sync()
+        print(f"Zsynchronizowano {len(synced)} komend slash")
+    except Exception as e:
+        print(e)
 
-# komenda prefix
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("🏓 Publiczny bot działa!")
 
 
-# slash command
 @bot.tree.command(name="ping", description="Sprawdza czy publiczny bot działa")
 async def slash_ping(interaction: discord.Interaction):
     await interaction.response.send_message("🏓 Publiczny bot działa!")
 
 
-@bot.event
-async def setup_hook():
-    await bot.tree.sync()
-
-
 if not TOKEN:
-    raise ValueError("Brak PUBLIC_DISCORD_TOKEN w .env")
+    raise ValueError("Brak PUBLIC_DISCORD_TOKEN w Railway Variables")
 
 bot.run(TOKEN)
